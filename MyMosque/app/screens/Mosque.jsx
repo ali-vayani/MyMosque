b41daf7f107418ad2448c766b13799a880d7a915
pAsta-kun <ila.00123ivay@gmail.com>
2023-07-25 22:26:34 -0500

    mosque page done

--- MyMosque/app/screens/Mosque.jsx
+++ MyMosque/app/screens/Mosque.jsx
@@ -1,4 +1,4 @@
-import {View, Text, Button, StyleSheet, Image, FlatList, TouchableOpacity} from 'react-native'
+import {View, Text, Button, StyleSheet, Image, ImageBackground, FlatList, TouchableOpacity} from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient';
 import PrayerBar from '../components/elements/prayerBar';
 import Location from '../components/elements/location';
@@ -18,19 +18,20 @@ const Mosque = ({navigation}) => {
             opacity: .1,
             }}/>
             <View style={styles.content}>
-                <Image style={styles.image}>
+                <View style={styles.image}>
                     <Text style={styles.mainText}>Watauga Masjid</Text>
                     <Text style={styles.minorText}>6005 Chapman Rd, Watauga, TX 76148 · 20 min drive </Text>
-                </Image>
+                </View>
                 <View style={styles.contentBlock}>
                     <Text style={styles.mainText}>Announcments</Text>
-                    <View>
+                    <View style={styles.announcmentInfo}>
                         <View style={styles.userIcon}></View> 
                         <View style={styles.userInfo}>
                             <Text style={styles.minorText}>Name</Text>
                             <Text style={styles.minorMinorText}>July 20th</Text>
                         </View>
                     </View>
+                        <Text style={styles.minorText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui. Nunc congue nisi vitae suscipit tellus mauris. Nunc eget lorem dolor sed.  </Text>
                 </View>
 
                 <View style={styles.contentBlock}>
@@ -73,35 +74,43 @@ const styles = StyleSheet.create({
     minorText: {
         fontSize: 14,
         fontWeight: 500,
-        color: '#FFF4D2',
+        color: '#FFF4D3',
     },
     mainText: {
         fontSize: 24,
         fontWeight: 'bold',
         color: '#FFF4D2',
     },
-    mainText: {
+    minorMinorText: {
         fontSize: 10,
         fontWeight: 500,
         color: '#FFF4D2',
     },
     image: {
         width: '100%',
-        height: '25$',
-        alignItems: 'flex-end',
+        height: '25%',
         flexDirection: 'column',
-        marginBottom: 25
+        backgroundColor: 'black',
+        justifyContent: 'flex-end',
+        paddingBottom: 15,
+        paddingLeft: 10,
+        marginBottom: 25,
+        shadowColor: 'black',
+        shadowOpacity: 1,
+        shadowRadius: 10
     },
     contentBlock:{
         width: '100%',
         height: '27%',
-        backgound: 'rgba(255, 244, 210, .1)',
+        backgroundColor: 'rgba(255, 244, 210, .1)',
         flexDirection: 'column',
-        marginBottom: 20
+        marginBottom: 20,
+        paddingHorizontal: 20,
+        paddingVertical: 15,
     },
     userIcon: {
-        background: 'rgb(255, 244, 210)',
-        borderRadius: 15,
+        backgroundColor: 'rgb(255, 244, 210)',
+        borderRadius: 35,
         width: 35,
         height: 35, 
     },
@@ -109,18 +118,24 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         width: '15%',
         height: 35,
+        //justifyContent:'center',
+        marginHorizontal: 5,
+    },
+    announcmentInfo: {
+        flexDirection: 'row',
         alignItems: 'center',
         marginTop: 20,
         marginBottom: 10,    
     },
     divideBar:{
-        height: 3,
+        height: 1,
         wdith: '90%',
-        background: 'rgb(255, 244, 210)',
+        backgroundColor: 'rgb(255, 244, 210)',
         marginVertical: '7%'
     },
     info:{
-        marginHorizontal: '5%',
+        marginHorizontal: 15,
+        marginVertical: 25,
         flexDirection: 'column',
 
     }
