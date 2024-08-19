3ee9ee9df15720118fdefbe403e9f1d3a8ea933e
ali v <112457969+ali-vayani@users.noreply.github.com>
2024-08-19 11:59:17 -0500

    posts added

--- MongoDB/models/mosque.js
+++ MongoDB/models/mosque.js
@@ -16,6 +16,24 @@ const mosqueSchema = new mongoose.Schema({
         maghrib: {type: String, required: true},
         isha: {type: String, required: true},
         endDate: {type: Date, required: true}
+    }],
+    posts: [{ 
+        image: {
+            type: Boolean,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            required: true
+        },
+        likes: {
+            type: Number,
+            required: true
+        }
     }]
 }, { collection: 'Mosques' });
 
