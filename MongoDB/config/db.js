3252a071df972cf97dceb5ccc9f36c993848150e
ali v <112457969+ali-vayani@users.noreply.github.com>
2025-01-25 19:01:37 -0600

    Delete MongoDB directory

--- MongoDB/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-const dbConnect = async (dbName) => {
-    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yukikwg.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=MyMosque`;
-    await mongoose.connect(uri); 
-}
-
-module.exports = dbConnect;
