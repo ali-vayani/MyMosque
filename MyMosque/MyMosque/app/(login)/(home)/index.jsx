1d5f8afb226f400c662d1ced6f77cc9d8f63d5e6
Ali Vayani <ali.vayani@utexas.edu>
2025-02-24 15:22:32 -0600

    app refresh working

--- MyMosque/MyMosque/app/(login)/(home)/index.jsx
+++ MyMosque/MyMosque/app/(login)/(home)/index.jsx
@@ -10,23 +10,23 @@ import { FIREBASE_APP, FIRESTORE_DB } from '../../../firebaseConfig';
 import getLocalPrayerTimes from '../../../functions/getLocalPrayerTimes';
 import getUserLocation from '../../../functions/getUserLocation';
 import getLocalMosques from '../../../functions/getLocalMosques';
-import * as BackgroundFetch from 'expo-background-fetch';
-import * as TaskManager from 'expo-task-manager';
+// import * as BackgroundFetch from 'expo-background-fetch';
+// import * as TaskManager from 'expo-task-manager';
 
-const BACKGROUND_FETCH_TASK = 'background-fetch';
-let now = new Date();
-TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
-    now = Date.now();
-    const currentDate = new Date(now);
-    console.log(`Got background fetch call at date: ${currentDate.toISOString()}`);
+// const BACKGROUND_FETCH_TASK = 'background-fetch';
+// let now = new Date();
+// TaskManager.defineTask(BACKGROUND_FETCH_TASK, async () => {
+//     now = Date.now();
+//     const currentDate = new Date(now);
+//     console.log(`Got background fetch call at date: ${currentDate.toISOString()}`);
     
-    // Update the last fetch time in the app
-    if (global.setLastFetchTime) {
-        global.setLastFetchTime(currentDate.toLocaleTimeString());
-    }
+//     // Update the last fetch time in the app
+//     if (global.setLastFetchTime) {
+//         global.setLastFetchTime(currentDate.toLocaleTimeString());
+//     }
 
-    return BackgroundFetch.BackgroundFetchResult.NewData;
-});
+//     return BackgroundFetch.BackgroundFetchResult.NewData;
+// });
 
 const Home = () => {
     const { uid } = useLocalSearchParams();
@@ -37,31 +37,31 @@ const Home = () => {
     const docRef = uid ? doc(FIRESTORE_DB, "users", uid) : null;
     const [masjidId, setMasjidId] = useState(["OnfodEG98Qaa3GIYKNxW"])
 
-    async function registerBackgroundFetchAsync() {
-        return BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
-            minimumInterval: 10,
-            stopOnTerminate: false, // android only,
-            startOnBoot: true, // android only
-        });
-    }
+    // async function registerBackgroundFetchAsync() {
+    //     return BackgroundFetch.registerTaskAsync(BACKGROUND_FETCH_TASK, {
+    //         minimumInterval: 10,
+    //         stopOnTerminate: false, // android only,
+    //         startOnBoot: true, // android only
+    //     });
+    // }
 
     useEffect(() => {
         const initializeApp = async () => {
             await getAppData();
-            await registerBackgroundFetchAsync();
+            // await registerBackgroundFetchAsync();
             
             // Make setLastFetchTime available globally for the background task
-            global.setLastFetchTime = setLastFetchTime;
+            // global.setLastFetchTime = setLastFetchTime;
 
             // Update current time every second
-            const timer = setInterval(() => {
-                setCurrentTime(new Date().toLocaleTimeString());
-            }, 1000);
+            // const timer = setInterval(() => {
+            //     setCurrentTime(new Date().toLocaleTimeString());
+            // }, 1000);
 
-            return () => {
-                clearInterval(timer);
-                global.setLastFetchTime = null;
-            };
+            // return () => {
+            //     clearInterval(timer);
+            //     global.setLastFetchTime = null;
+            // };
         };
         initializeApp();
     }, []);
@@ -89,12 +89,6 @@ const Home = () => {
         <View style={styles.page}>
             <LinearGradient colors={['#67519A', '#57658E', '#679159']} style={styles.background}/>
             <View style={styles.content}>
-                <View style={styles.timeContainer}>
-                    <Text style={styles.timeText}>Current Time: {currentTime}</Text>
-                    {lastFetchTime && (
-                        <Text style={styles.fetchText}>Last Background Fetch: {lastFetchTime}</Text>
-                    )}
-                </View>
                 {locationData && <PrayerTimesWidget uid={uid} favMasjids={masjidId} locationData={locationData}/>}
                 {locMosques && <SearchWidget uid={uid} locMosques={locMosques}/>}
                 {masjidId && locationData ? (
